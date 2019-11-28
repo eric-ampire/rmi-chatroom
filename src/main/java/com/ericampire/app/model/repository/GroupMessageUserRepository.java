1d14ab88adc0ef123c5aca770ef34475bcf8f516
Eric Ampire <ericampire.top@gmail.com>
2019-11-28 23:22:18 +0200

    Adding Repository for jointures table
    Signed-off-by: Eric Ampire <ericampire.top@gmail.com>

---
+++ src/main/java/com/ericampire/app/model/repository/GroupMessageUserRepository.java
@@ -0,0 +1,7 @@
+package com.ericampire.app.model.repository;
+
+import com.ericampire.app.model.entity.GroupMessageUser;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GroupMessageUserRepository extends CrudRepository<GroupMessageUser, Long> {
+}
---
+++ src/main/java/com/ericampire/app/model/repository/GroupUserRepository.java
@@ -0,0 +1,7 @@
+package com.ericampire.app.model.repository;
+
+import com.ericampire.app.model.entity.GroupUser;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GroupUserRepository extends CrudRepository<GroupUser, Long> {
+}
