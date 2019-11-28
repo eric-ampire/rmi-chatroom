75bb1a9d5dc4021099b2f81000053dc08b73a325
Eric Ampire <ericampire.top@gmail.com>
2019-11-28 10:59:39 +0200

    Adding spring
    Signed-off-by: Eric Ampire <ericampire.top@gmail.com>

--- src/main/java/com/ericampire/app/Main.java
+++ src/main/java/com/ericampire/app/Main.java
@@ -5,9 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main extends Application {
     public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
         Application.launch(args);
     }
 
