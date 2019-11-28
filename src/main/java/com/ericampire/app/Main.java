ec00480915d7a2a61d7b22dc351ac717a2afff44
Eric Ampire <ericampire.top@gmail.com>
2019-11-28 15:22:30 +0200

    Spring and JavaFX configuration
    Signed-off-by: Eric Ampire <ericampire.top@gmail.com>

--- src/main/java/com/ericampire/app/Main.java
+++ src/main/java/com/ericampire/app/Main.java
@@ -7,19 +7,39 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Main extends Application {
+    private ConfigurableApplicationContext springContext;
+    private Parent rootNode;
+    private FXMLLoader fxmlLoader;
+
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
         Application.launch(args);
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void init() throws Exception {
+        springContext = SpringApplication.run(Main.class);
+        fxmlLoader = new FXMLLoader();
+        fxmlLoader.setControllerFactory(springContext::getBean);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/view/main.fxml"));
+        fxmlLoader.setLocation(getClass().getResource("/view/main.fxml"));
+        rootNode = fxmlLoader.load();
+
         stage.setTitle("Chat-Room");
-        stage.setScene(new Scene(root));
+        stage.setScene(new Scene(rootNode));
+        stage.setResizable(false);
         stage.show();
+    }
+
+    @Override
+    public void stop() {
+        springContext.stop();
     }
 }
