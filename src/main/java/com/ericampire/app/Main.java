88e23c6e5f33c06c390a1ff92d12c20b6e0933f1
Eric Ampire <ericampire.top@gmail.com>
2019-11-28 09:47:17 +0200

    Loading FXML file
    Signed-off-by: Eric Ampire <ericampire.top@gmail.com>

--- src/main/java/com/ericampire/app/Main.java
+++ src/main/java/com/ericampire/app/Main.java
@@ -1,7 +1,21 @@
 package com.ericampire.app;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Salut les amis");
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/main.fxml"));
+        stage.setTitle("Chat-Room");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
